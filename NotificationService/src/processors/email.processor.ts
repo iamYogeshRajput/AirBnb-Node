98b310192bf36ae398b4df6f20e95f8022862257
Yogesh Rajput <yogeshrajput7433@gmail.com>
2026-02-09 18:20:06 +0530

    Updated basic functionalities in notification service

---
+++ NotificationService/src/processors/email.processor.ts
@@ -0,0 +1,32 @@
+import { Job, Worker } from "bullmq";
+import { getRedisConnObject } from "../config/redis.config";
+import { NotificationDTO } from "../dto/notification.dto";
+import { MAILER_QUEUE } from "../queues/mailer.queue";
+import { MAILER_PAYLOAD } from "../producers/email.producer";
+
+export const setupMailerWorker = () => {
+
+  const emailProcessor = new Worker<NotificationDTO>(
+    MAILER_QUEUE,
+    async (job : Job) => {
+        if(job.name !== MAILER_PAYLOAD){
+            throw new Error("Invalid job name");
+        }
+        // call the service layer from here
+        const  payload = job.data;
+        console.log(`Processing email with payload: ${JSON.stringify(payload)}`);
+        
+    },
+    {
+      connection: getRedisConnObject(),
+    },
+  );
+
+  emailProcessor.on("failed", (job) => {
+    console.error("Email processing failed");
+  });
+
+  emailProcessor.on("completed", (job) => {
+    console.log("Email processing completed");
+  });
+}
--- NotificationService/src/server.ts
+++ NotificationService/src/server.ts
@@ -1,3 +1,4 @@
+import { NotificationDTO } from './dto/notification.dto';
 import express from 'express';
 import { serverConfig } from './config';
 import v1Router from './routers/v1/index.router';
@@ -5,6 +6,8 @@ import v2Router from './routers/v2/index.router';
 import { appErrorHandler, genericErrorHandler } from './middlewares/error.middleware';
 import logger from './config/logger.config';
 import { attachCorrelationIdMiddleware } from './middlewares/correlation.middleware';
+import { setupMailerWorker } from './processors/email.processor';
+import { addEmailToQueue } from './producers/email.producer';
 const app = express();
 
 app.use(express.json());
@@ -29,4 +32,17 @@ app.use(genericErrorHandler);
 app.listen(serverConfig.PORT, () => {
     logger.info(`Server is running on http://localhost:${serverConfig.PORT}`);
     logger.info(`Press Ctrl+C to stop the server.`);
+    setupMailerWorker();
+    logger.info(`Mailer worker setup completed.`);
+
+    const sampleNotification: NotificationDTO = {
+        to: "sample",
+        subject : "Sample Email",
+        templateId: "sample_template",
+        params: {
+            name: "Yogesh",
+            orderId: "12345",
+        }
+    }
+    addEmailToQueue(sampleNotification);
 });
